edd97fa6c508473d94b240374617e07a0771fc0b
Damian B <s178929@student.uken.krakow.pl>
2026-03-24 19:10:47 +0100

    Lab-04 praca na zajeciach

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,6 +7,14 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+
+  List<Task> tasks = [
+    Task(title: "Zadanie z fluttera", deadline: "za tydzień", done: false),
+    Task(title: "Posprzątać w domu", deadline: "jutro", done: true),
+    Task(title: "Zrobić pranie", deadline: "dzisiaj", done: true),
+    Task(title: "Zrobić zakupy", deadline: "za dwa dni", done: true)
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,16 +23,66 @@ class MyApp extends StatelessWidget {
           title: Text("KrakFlow"),
           centerTitle: true,
         ),
-        body: Center(
+        body: Padding(
+          padding: EdgeInsets.all(150),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Text("KrakFlow"),
-              Text("Organizacja studiów"),
-              Text("Dzisiejsze zadania"),
+              Text("Masz dziś ${tasks.length} zadania"),
+              SizedBox(height: 20),
+              Text(
+                "Dzisiejsze zadania",
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: tasks.length,
+                  itemBuilder: (context, index){
+                    return Column(children: [
+                      Text(tasks[index].title),
+                      Text(tasks[index].deadline),
+                    ],
+                    );
+                  },
+                ),
+              ),
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class Task {
+  final String title;
+  final String deadline;
+  final bool done;
+  Task({required this.title, required this.deadline, required this.done});
+}
+
+class TaskCard extends StatelessWidget {
+  final String title;
+  final String subtitle;
+  final IconData icon;
+
+  const TaskCard({
+    super.key,
+    required this.title,
+    required this.subtitle,
+    required this.icon,
+  });
+
+  @override
+  Widget build(BuildContext context){
+    return Card(
+      child: ListTile(
+        leading: Icon(icon),
+        title: Text(title),
+        subtitle: Text(subtitle),
       ),
     );
   }
