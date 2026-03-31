0942d820fe865a56866f631c17457b49a704dbed
Damian Bijak <s178929@student.uken.krakow.pl>
2026-03-31 14:45:46 +0200

    lab_04 krakflow task list poprawione

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,14 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+
+  List<Task> tasks = [
+    Task(title: "Zadanie z fluttera", deadline: "za tydzień", done: false, priority: "wysoki"),
+    Task(title: "Posprzątać w domu", deadline: "jutro", done: true, priority: "niski"),
+    Task(title: "Zrobić pranie", deadline: "dzisiaj", done: true, priority: "niski"),
+    Task(title: "Zrobić zakupy", deadline: "za dwa dni", done: false, priority: "średni")
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,16 +21,71 @@ class MyApp extends StatelessWidget {
           title: Text("KrakFlow"),
           centerTitle: true,
         ),
-        body: Center(
+        body: Padding(
+          padding: EdgeInsets.all(150),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Text("KrakFlow"),
-              Text("Organizacja studiów"),
-              Text("Dzisiejsze zadania"),
+              Text("Masz dziś ${tasks.length} zadania"),
+              SizedBox(height: 20),
+              Text(
+                "Dzisiejsze zadania",
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 10),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: tasks.length,
+                  itemBuilder: (context, index) {
+                    final task = tasks[index];
+                    return TaskCard(
+                      title: task.title,
+                      subtitle: "${task.deadline} | Priorytet: ${task.priority}",
+                      icon: task.done
+                          ? Icons.check_circle
+                          : Icons.radio_button_unchecked,
+                    );
+                  },
+                ),
+              ),
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class Task {
+  final String title;
+  final String deadline;
+  final bool done;
+  final String priority;
+  Task({required this.title, required this.deadline, required this.done, required this.priority,});
+}
+
+class TaskCard extends StatelessWidget {
+  final String title;
+  final String subtitle;
+  final IconData icon;
+
+  const TaskCard({
+    super.key,
+    required this.title,
+    required this.subtitle,
+    required this.icon,
+  });
+
+  @override
+  Widget build(BuildContext context){
+    return Card(
+      child: ListTile(
+        leading: Icon(icon),
+        title: Text(title),
+        subtitle: Text(subtitle),
       ),
     );
   }
