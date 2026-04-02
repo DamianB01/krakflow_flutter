fe5a99d3c5cde743005ed0cd26d1ddba90e17db2
Damian Bijak <s178929@student.uken.krakow.pl>
2026-04-02 17:50:16 +0200

    lab 5 - add animation

--- lib/main.dart
+++ lib/main.dart
@@ -70,12 +70,8 @@ class _HomeScreenState extends State<HomeScreen> {
                   AddTaskScreen(),
               transitionsBuilder:
                   (context, animation, secondaryAnimation, child) {
-                final offsetAnimation = Tween<Offset>(
-                  begin: Offset(1.0, 0.0),
-                  end: Offset.zero,
-                ).animate(animation);
-                return SlideTransition(
-                  position: offsetAnimation,
+                return FadeTransition(
+                  opacity: animation,
                   child: child,
                 );
               },
